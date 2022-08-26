1db4e1ac33749429efd300ee37cd5c26923c0c19
Martin Hell <martinhellapp@gmail.com>
2022-08-26 16:42:50 +0200

    red_fly using new player_detector module

--- tiny_wizard/enemies/red_fly/red_fly_behavior.gd
+++ tiny_wizard/enemies/red_fly/red_fly_behavior.gd
@@ -3,11 +3,12 @@ extends QuiverCharacterBehavior
 
 const CHANGE_DIRECTION_TIME = 2.0
 
-var _target_player = null
 var _t := randf()*CHANGE_DIRECTION_TIME
 
 var _random_movement := Vector2.ZERO
 var _player_movement := Vector2.ZERO
+
+@onready var player_detector := $PlayerDetector 
 
 func _process(delta):
 	_t -= delta
@@ -15,17 +16,10 @@ func _process(delta):
 		_random_movement = Vector2.RIGHT.rotated(randf()*2*PI) 
 		_t = CHANGE_DIRECTION_TIME
 	
-	if _target_player != null:
-		_player_movement = 0.33*global_position.direction_to(_target_player.global_position)
+	if player_detector.player_is_in_range():
+		_player_movement = 0.33*global_position.direction_to(player_detector.get_player_position())
 	else:
 		_player_movement = Vector2.ZERO
 	
 	action.moving_direction = _random_movement + 5*_player_movement
 
-
-func _on_player_detection_area_body_entered(body):
-	_target_player = body
-
-
-func _on_player_detection_area_body_exited(body):
-	_target_player = null
